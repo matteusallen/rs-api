4e6afe050ce9dfdf0d684a9c127d3a08b2362183
Matthew Pittmann <matayo@tayoallen.io>
2023-08-26 12:25:35 -0500

    Delete routes directory

--- routes/notifications.js
+++
@@ -1,8 +0,0 @@
-import { registerSubscriber, notifySubscriber } from 'Lib/pushNotifications';
-
-const notificationRoutes = app => {
-  app.post('/subscription', registerSubscriber);
-  app.get('/subscription/:id', notifySubscriber);
-};
-
-export default notificationRoutes;
